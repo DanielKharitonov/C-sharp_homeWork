0c09eda576d3cb5b405c3ad7b2ae3980ea97a024
Daniel Kharitnov <Daniel.Kharitnov@gmail.com>
2023-01-22 18:58:24 +0500

    Added Task1(10) in folder Homework2

---
+++ Homework2/Task1(10)/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+
+System.Console.WriteLine("Введите трехзначное число:");
+string Enter = System.Console.ReadLine()!;
+if (int.TryParse(Enter, out int number) && Enter.Length == 3)
+{
+    System.Console.WriteLine($"Вторая цифра введенного числа: {Enter[1]}");
+}
+else
+{
+    System.Console.WriteLine("Введен текст или некорректное число");
+}
