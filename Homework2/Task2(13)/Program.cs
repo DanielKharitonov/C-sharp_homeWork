43d757e3dac9dd32022f1c9a39615bd9ad56f0c8
Daniel Kharitnov <Daniel.Kharitnov@gmail.com>
2023-01-22 18:58:46 +0500

    Added Task2(13) in folder Homework2

---
+++ Homework2/Task2(13)/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+
+System.Console.Write("Введите число: ");
+string enter = System.Console.ReadLine()!;
+
+if (int.TryParse(enter, out int number) && number > 99)
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    System.Console.WriteLine($"Третья цифра введенного числа: {number % 10}");
+}
+else
+{
+    System.Console.WriteLine("Введен текст или некорректное число");
+}
