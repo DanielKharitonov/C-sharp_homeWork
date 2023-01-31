5c78cc92a002cf6108802c699bcc2389966c2e35
Daniel Kharitnov <Daniel.Kharitnov@gmail.com>
2023-01-31 21:41:26 +0500

    Added Task2(27)

---
+++ Homework4/Task2(27)/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+
+uint EnterNumberNatur(string message)
+{
+    uint number;
+    while (true)
+    {
+        System.Console.Write(message);
+        if (!(uint.TryParse(Console.ReadLine(), out number)))
+        {
+            System.Console.WriteLine("Введено некорректное число, попробуйте еще раз.");
+        }
+        else
+        {
+            break;
+        }
+    }
+    return number;
+}
+int SumNumbersNumber(string number)
+{
+    int Sum = 0;
+    for (int i = 0; i < number.Length; i++)
+    {
+        Sum = Sum + Convert.ToInt32(Convert.ToString(number[i]));
+    }
+    return Sum;
+}
+
+string A = Convert.ToString(EnterNumberNatur("Введите натуральное число: "));
+Console.Clear();
+System.Console.WriteLine($"Сумма цифр числа {A} = {SumNumbersNumber(A)}");
