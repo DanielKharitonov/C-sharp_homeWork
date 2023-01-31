594db1d1c21e1d52da5bc91a4c037cd07a7858d4
Daniel Kharitnov <Daniel.Kharitnov@gmail.com>
2023-01-31 21:41:49 +0500

    Added Task3(29)

---
+++ Homework4/Task3(29)/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Clear();
+
+uint EnterNumberNatur(string message)
+{
+    uint number;
+    while (true)
+    {
+        System.Console.Write(message);
+        if (!(uint.TryParse(Console.ReadLine(), out number)))
+        {
+            System.Console.WriteLine("Введено некорректное число, попробуйте еще раз.");
+        }
+        else
+        {
+            break;
+        }
+    }
+    return number;
+}
+void PrintFillArray(int[] array)
+{
+   for (int i=0;i<array.Length;i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+    System.Console.WriteLine("Массив: [" + string.Join(", ", array) + "]");
+}
+
+uint size = EnterNumberNatur("Введите размер массива: ");
+Console.Clear();
+PrintFillArray(new int[size]);
+
